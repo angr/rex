e0fbe78f2b8c0118d3a7e8cefe5df95f321ef37b
Nick Stephens <nick.d.stephens@gmail.com>
2015-10-07 17:32:24 -0700

    Now install requirements in setup.py and include a requirements.txt file

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from distutils.core import setup
 setup(name='rex',
       version='0.01',
-      packages=['rex', 'rex.exploit', 'rex.exploit.cgc', 'rex.exploit.cgc.payload', 'rex.exploit.techniques', 'rex.exploit.shellcodes'])
+      packages=['rex', 'rex.exploit', 'rex.exploit.cgc', 'rex.exploit.cgc.payload', 'rex.exploit.techniques', 'rex.exploit.shellcodes'],
+      install_requires=[
+            'angr',
+            'simuvex',
+            'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer',
+            'git+ssh://git@git.seclab.cs.ucsb.edu:/angr/angrop.git#egg=angrop',
+      ],
+)
