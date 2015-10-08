e66adafce5cbef573bb2df4a1c55fe705d4a894c
Nick Stephens <nick.d.stephens@gmail.com>
2015-10-07 22:15:34 -0700

    Reorder parameters in setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
       name='rex',
       version='0.01',
       packages=['rex', 'rex.exploit', 'rex.exploit.cgc', 'rex.exploit.cgc.payload', 'rex.exploit.techniques', 'rex.exploit.shellcodes'],
+      dependency_links=[
+            'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer-0.1',
+            'git+ssh://git@git.seclab.cs.ucsb.edu:/angr/angrop.git#egg=angrop-1.0',
+      ],
       install_requires=[
             'angr',
             'simuvex',
             'tracer==0.1',
             'angrop==1.0',
-      ],
-      dependency_links=[
-            'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer-0.1',
-            'git+ssh://git@git.seclab.cs.ucsb.edu:/angr/angrop.git#egg=angrop-1.0',
       ],
 )
