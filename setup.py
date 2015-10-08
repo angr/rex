4ae694bd1fab529fc787aa6a4894c4a6357cf3ed
Nick Stephens <nick.d.stephens@gmail.com>
2015-10-07 22:12:44 -0700

    Really add version numbers

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
             'angr',
             'simuvex',
             'tracer==0.1',
-            'angrop==0.1',
+            'angrop==1.0',
       ],
       dependency_links=[
             'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer-0.1',
-            'git+ssh://git@git.seclab.cs.ucsb.edu:/angr/angrop.git#egg=angrop-0.1',
+            'git+ssh://git@git.seclab.cs.ucsb.edu:/angr/angrop.git#egg=angrop-1.0',
       ],
 )
