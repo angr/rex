7183f865f2a160b2b953d8c5b1d566bb8552d2a6
Nick Stephens <nick.d.stephens@gmail.com>
2015-10-07 22:44:24 -0700

    Shell out to pip to install dependencies from private repos, this is pretty gross, but other options didn't appear to work

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from distutils.core import setup
+import subprocess
+
+TRACER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer'
+ANGROP_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/angr/angrop.git#egg=angrop'
+
+# this is really gross, but you do what you gotta do
+if subprocess.call(['pip', 'install', TRACER_URL]) != 0:
+    raise LibError("Unable to install tracer")
+
+if subprocess.call(['pip', 'install', ANGROP_URL]) != 0:
+    raise LibError("Unable to install angrop")
+
 
 setup(
       name='rex',
       version='0.01',
       packages=['rex', 'rex.exploit', 'rex.exploit.cgc', 'rex.exploit.cgc.payload', 'rex.exploit.techniques', 'rex.exploit.shellcodes'],
-      dependency_links=[
-            'git+ssh://git@git.seclab.cs.ucsb.edu/cgc/tracer.git#egg=tracer-0.1',
-            'git+ssh://git@git.seclab.cs.ucsb.edu/angr/angrop.git#egg=angrop-1.0',
-      ],
       install_requires=[
             'angr',
             'simuvex',
-            'tracer==0.1',
-            'angrop==1.0',
+            'tracer',
+            'angrop',
       ],
 )
