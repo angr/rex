f6b01d07c50019f7702528e96884ed5310c8ae5c
Yan <yans@yancomm.net>
2015-10-13 22:31:33 -0700

    remove messy pip

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 import subprocess
 
-TRACER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer'
-ANGROP_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/angr/angrop.git#egg=angrop'
-
-# this is really gross, but you do what you gotta do
-if subprocess.call(['pip', 'install', TRACER_URL]) != 0:
-    raise LibError("Unable to install tracer")
-
-if subprocess.call(['pip', 'install', ANGROP_URL]) != 0:
-    raise LibError("Unable to install angrop")
+#TRACER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer'
+#ANGROP_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/angr/angrop.git#egg=angrop'
+#
+## this is really gross, but you do what you gotta do
+#if subprocess.call(['pip', 'install', TRACER_URL]) != 0:
+#   raise LibError("Unable to install tracer")
+#
+#if subprocess.call(['pip', 'install', ANGROP_URL]) != 0:
+#   raise LibError("Unable to install angrop")
 
 
 setup(
