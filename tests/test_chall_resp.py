c76d49a6bc3ee0136a0d2810a6f1f5749b4cd22a
Chris Salls <salls@cs.ucsb.edu>
2016-07-05 00:43:43 -0700

    update test

--- tests/test_chall_resp.py
+++ tests/test_chall_resp.py
@@ -4,6 +4,15 @@ import nose
 import os
 bin_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries-private'))
 
+
+def _do_pov_test(pov, enable_randomness=True):
+    """ Test a POV """
+    for _ in range(10):
+        if pov.test_binary(enable_randomness=enable_randomness):
+            return True
+    return False
+
+
 def test_chall_response():
     crash_input = "465f2770".decode('hex') + \
                   "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" \
@@ -12,9 +21,9 @@ def test_chall_response():
     crash = rex.Crash(bin_location + "/tests/i386/overflow_after_challenge_response2", crash=crash_input)
     exploit_f = crash.exploit()
     for e in exploit_f.register_setters:
-        assert e.test_binary()
+        nose.tools.assert_true(_do_pov_test(e))
     for e in exploit_f.leakers:
-        assert e.test_binary()
+        nose.tools.assert_true(_do_pov_test(e))
 
 
 def run_all():
