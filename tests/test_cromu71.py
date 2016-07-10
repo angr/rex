aba187459e0c7f4f3cd91bbdb43bc55bc1cc970e
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-09 20:50:12 -0700

    Add CROMU_00071 testcase

---
+++ tests/test_cromu71.py
@@ -0,0 +1,46 @@
+import os
+import rex
+import nose
+
+import logging
+l = logging.getLogger("rex").setLevel("DEBUG")
+
+bin_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries-private'))
+test_data_location = str(os.path.dirname(os.path.realpath(__file__)))
+
+def _do_pov_test(pov, enable_randomness=True):
+    ''' Test a POV '''
+    for _ in range(10):
+        if pov.test_binary(enable_randomness=enable_randomness):
+            return True
+    return False
+
+def test_cromu_00071():
+    '''
+    Test exploitation of CROMU_00071
+    '''
+
+    crash = rex.Crash(os.path.join(bin_location, "cgc_trials/CROMU_00071"), "0c0c492a53acacacacacacacacacacacacac000100800a0b690e0aef6503697d660a0059e20afc0a0a332f7d66660a0059e20afc0a0a332f7fffffff16fb1616162516161616161616166a7dffffff7b0e0a0a6603697d660a0059e21c".decode('hex'))
+
+    arsenal = crash.exploit()
+
+    nose.tools.assert_equals(len(arsenal.register_setters), 2)
+
+    for exploit in arsenal.register_setters:
+        nose.tools.assert_true(_do_pov_test(exploit))
+
+def run_all():
+    functions = globals()
+    all_functions = dict(filter((lambda (k, v): k.startswith('test_')), functions.items()))
+    for f in sorted(all_functions.keys()):
+        if hasattr(all_functions[f], '__call__'):
+            all_functions[f]()
+
+
+if __name__ == "__main__":
+
+    import sys
+    if len(sys.argv) > 1:
+        globals()['test_' + sys.argv[1]]()
+    else:
+        run_all()
