aef70c597fa4c1279e0081808384f32e59a31389
Owlz <whootandahalf@gmail.com>
2017-03-12 22:07:37 -0400

    Removing LAZY_SOLVES

--- rex/crash.py
+++ rex/crash.py
@@ -91,7 +91,7 @@ class Crash(object):
         if crash_state is None:
             # run the tracer, grabbing the crash state
             remove_options = {so.TRACK_REGISTER_ACTIONS, so.TRACK_TMP_ACTIONS, so.TRACK_JMP_ACTIONS,
-                              so.ACTION_DEPS, so.TRACK_CONSTRAINT_ACTIONS}
+                              so.ACTION_DEPS, so.TRACK_CONSTRAINT_ACTIONS, so.LAZY_SOLVES}
             add_options = {so.MEMORY_SYMBOLIC_BYTES_MAP, so.TRACK_ACTION_HISTORY, so.CONCRETIZE_SYMBOLIC_WRITE_SIZES,
                            so.CONCRETIZE_SYMBOLIC_FILE_READ_SIZES}
 
