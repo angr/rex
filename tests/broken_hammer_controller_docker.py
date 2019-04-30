099facd5bf2b7530f00b55d543dbbde0ff4c8e57
Audrey Dutcher <audrey@rhelmot.io>
2019-04-30 14:25:44 -0700

    fix the hammer controller test. unfortunately it cannot be fixed.

--- tests/broken_hammer_controller_docker.py
+++ tests/broken_hammer_controller_docker.py
@@ -1,29 +1,30 @@
 import os
+import nose
 import archr
 import rex
+import logging
+import subprocess
 
+tests_location = os.path.dirname(os.path.realpath(__file__))
 
 def build_docker():
-    os.system('make -C ./hammer_controller')
-
+    subprocess.check_call(['make'], cwd=os.path.join(tests_location, 'hammer_controller'))
 
 def test_hammer_controller_rr_tracer():
     try:
         import trraces
-    except:
-        return
+    except ImportError:
+        raise nose.SkipTest('need trraces')
 
     build_docker()
 
     t = archr.targets.DockerImageTarget('rex_tests:hammer_controller').build().start()
     tb = archr.arsenal.RRTracerBow(t, local_trace_dir='/tmp/rex_hammer_controller_trace', symbolic_fd=0)
 
-    import logging
-    logging.getLogger("angr.exploration_techniques.tracer").setLevel(logging.DEBUG)
-
     crash = rex.Crash(t, b"\x41"*120 + b'\n', aslr=False, tracer_bow=tb)
 
     exploit = crash.exploit()
+    assert 'rop_chess_control' in exploit.arsenal
     exploit.arsenal['rop_chess_control'].script()
     exploit.arsenal['rop_chess_control'].script("x2.py")
 
@@ -34,21 +35,25 @@ def test_hammer_controller_qemu_tracer():
     t = archr.targets.DockerImageTarget('rex_tests:hammer_controller').build().start()
     tb = archr.arsenal.QEMUTracerBow(t)
 
-    import logging
-    logging.getLogger("angr.exploration_techniques.tracer").setLevel(logging.DEBUG)
-
     crash = rex.Crash(t, b"\x41"*120 + b'\n', aslr=False, tracer_bow=tb)
 
     exploit = crash.exploit()
+    assert 'rop_chess_control' in exploit.arsenal
     exploit.arsenal['rop_chess_control'].script()
     exploit.arsenal['rop_chess_control'].script("x2.py")
 
 
-def run_all_tests():
+def run_all():
+    logging.getLogger("angr.exploration_techniques.tracer").setLevel(logging.DEBUG)
+    logging.getLogger("rex").setLevel(logging.DEBUG)
+
     for f_name in globals():
         if f_name.startswith('test_'):
-            globals()[f_name]()
+            try:
+                globals()[f_name]()
+            except nose.SkipTest:
+                pass
 
 
 if __name__ == '__main__':
-    run_all_tests()
+    run_all()
