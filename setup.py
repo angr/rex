607d00782926d449e637f9513697199536215a2c
balbassam <balbassam@users.noreply.github.com>
2019-06-05 19:50:12 -0700

    Better setup.py automation

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+import os
 import subprocess
 
 #TRACER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer'
@@ -11,16 +11,30 @@ import subprocess
 #if subprocess.call(['pip', 'install', ANGROP_URL]) != 0:
 #   raise LibError("Unable to install angrop")
 
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    packages = [x.strip('./').replace('/', '.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
 
 setup(
       name='rex',
       version='0.02',
-      packages=['rex', 'rex.exploit', 'rex.exploit.cgc', 'rex.exploit.cgc.type1', 'rex.exploit.cgc.type2', 'rex.exploit.cgc.c_templates', 'rex.exploit.techniques', 'rex.exploit.shellcodes'],
+      packages=packages,
       install_requires=[
             'angr',
             'povsim',
             'tracer',
             'angrop',
             'compilerex',
+            'archr',
+      ],
+      dependency_links=[
+          'git+ssh://git@github.com/mechaphish/compilerex#egg=compilerex',
+          'git+ssh://git@github.com/mechaphish/povsim#egg=povsim',
+          'git+ssh://git@github.com/angr/archr#egg=archr',
+          'git+ssh://git@github.com/angr/tracer#egg=tracer',
       ],
 )
