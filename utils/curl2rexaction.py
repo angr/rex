8884b8bb7af89b0c45188e91c68cf12f618cb088
Fish <fishw@asu.edu>
2021-02-26 02:01:42 -0700

    curl2rexaction: Refactor and fix some small issues.

--- utils/curl2rexaction.py
+++ utils/curl2rexaction.py
@@ -1,28 +1,16 @@
 import argparse
-import struct
-import sys
 import os
 import time
 import signal
 import psutil
 
-#from pwn import *
 import nclib
-#from nclib import Process
+from rex.exploit.actions import RexWaitAction, RexOpenChannelAction, RexSendAction, RexCloseChannelAction
 
-VERBOSE = False
-
-parser = argparse.ArgumentParser(description="Translate a bash script full of curl requests to rex actions, assuming the curl commands request 127.0.0.1")
-parser.add_argument('script', type=str, help="the path to the bash script")
-parser.add_argument('port', type=int, help="which port the curl commands send requests to")
-
-args = parser.parse_args()
-port = args.port
-script_path = args.script
-assert os.path.exists(script_path), "<script> is a bash script"
 
 class TimeoutError(Exception):
     pass
+
 
 def timeout(seconds_before_timeout):
     def decorate(f):
@@ -45,10 +33,6 @@ def timeout(seconds_before_timeout):
         return new_f
     return decorate
 
-r = nclib.Process(["sudo", "nc", "-l", str(port)], verbose=VERBOSE)
-p = nclib.Process(script_path, env={"PATH": os.path.dirname(os.path.abspath(__file__))+":"+os.getenv("PATH")}, verbose=VERBOSE)
-#print({ "PATH": os.path.dirname(os.path.abspath(__file__))+":"+os.getenv("PATH")})
-
 @timeout(2)
 def recv_once(r):
     return r.recv()
@@ -58,34 +42,70 @@ def recvall(r):
     while True:
         try:
             data += recv_once(r)
-        except Exception as e:
+        except Exception as ex:
             return data
 
 def force_kill(r):
     r.close()
     proc = psutil.Process(r.pid)
     for child in proc.children():
-        os.system("sudo kill -9 %d" % child.pid)
-    os.system("sudo kill -9 %d" % r.pid)
+        os.system("kill -9 %d" % child.pid)
+    os.system("kill -9 %d" % r.pid)
 
-# The assumption is curl is blocking, which is the default behavior
-requests = []
-while p.poll() is None:
-    data = recvall(r)
-    if data:
-        requests.append(data)
+
+def script2rexactions(script_path, ip="127.0.0.1", port=8000, VERBOSE=False):
+    assert os.path.isfile(script_path), f"{script_path} does not exist."
+
+    r = nclib.Process(["nc", "-l", ip, str(port)], verbose=VERBOSE)
+    p = nclib.Process([script_path, ip, str(port)],
+                      env={
+                          "PATH": os.path.dirname(os.path.abspath(__file__)) + ":" + os.getenv("PATH"),
+                          "TERM": "linux",
+                          "TERMINFO": "/etc/terminfo",
+                      },
+                      verbose=VERBOSE,
+                      )
+    # print({ "PATH": os.path.dirname(os.path.abspath(__file__))+":"+os.getenv("PATH")})
+
+    # The assumption is curl is blocking, which is the default behavior
+    requests = []
+    while p.poll() is None:
+        data = recvall(r)
+        if data:
+            requests.append(data)
+        force_kill(r)
+        r = nclib.Process(["nc", "-l", ip, str(port)], verbose=VERBOSE)
+    p.kill()
     force_kill(r)
-    r = nclib.Process(["sudo", "nc", "-l", str(port)])
-p.kill()
-force_kill(r)
 
-actions = ["RexWaitAction(5)"]
-for req in requests:
-    open_act = ""
-    actions += ["RexOpenChannelAction()", f"RexSendAction({repr(req)})", "RexCloseChannelAction()", "RexWaitAction(1)"]
-actions = actions[:-2] + [actions[-1]] # we don't close the last connection but we do wait after exploitation
+    actions = [RexWaitAction(2)]
+    for req in requests:
+        actions += [
+            RexOpenChannelAction(),
+            RexSendAction(req),
+            RexCloseChannelAction(),
+            RexWaitAction(1)
+        ]
+    actions = actions[:-2] + [actions[-1]]  # we don't close the last connection but we do wait after exploitation
+    return actions
 
-action_str = "actions = [" + ", ".join(actions) + "]"
 
-print(action_str)
+def main():
+    VERBOSE = False
 
+    parser = argparse.ArgumentParser(description="Translate a bash script full of curl requests to rex actions.")
+    parser.add_argument('script', type=str, help="the path to the bash script")
+    parser.add_argument("--ip", type=str, help="the IP that the script sends requests to", default="127.0.0.1")
+    parser.add_argument('--port', type=int, help="which port the script sends requests to", default=8000)
+
+    args = parser.parse_args()
+    ip = args.ip
+    port = args.port
+    script_path = args.script
+
+    actions = script2rexactions(script_path, ip=ip, port=port, VERBOSE=VERBOSE)
+    print(actions)
+
+
+if __name__ == "__main__":
+    main()
