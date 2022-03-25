453e3fcccfc910d543879cee828374a1345dd536
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2022-03-25 15:04:47 -0700

    Removed nose imports in test_chall_resp.py (#86)
    * Removed nose imports in test_chall_resp.py
    
    * Removed nose imports in test_explore.py
    
    * Fixed lint errors in test_explorer.py

--- tests/test_explore.py
+++ tests/test_explore.py
@@ -1,11 +1,7 @@
-
 import os
 
-import nose
-
-import rex
 import archr
-
+import rex
 
 bin_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries'))
 cache_location = str(os.path.join(bin_location, 'tests_data/rop_gadgets_cache'))
@@ -18,12 +14,13 @@ def test_write_what_where_shadowstack():
     path = os.path.join(bin_location, "tests/cgc/write_what_where_shadow_stack")
 
     with archr.targets.LocalTarget([path], target_os='cgc') as target:
-        crash = rex.Crash(target, crash=inp, rop_cache_path=os.path.join(cache_location, "write_what_where_shadow_stack"))
+        crash = rex.Crash(target, crash=inp, rop_cache_path=os.path.join(cache_location,
+            "write_what_where_shadow_stack"))
         arsenal = crash.exploit()
         crash.project.loader.close()
 
         exploit = arsenal.best_type2
-        nose.tools.assert_true(exploit.test_binary())
+        assert exploit.test_binary()
 
 
 def run_all():
